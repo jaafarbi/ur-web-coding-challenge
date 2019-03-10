f256a9c05e3b6642528b6cd05b9fbd55c23cdb66
Jaafar Biyadi <jaafar.biyadi@gmail.com>
2019-03-11 00:07:50 +0100

    Added Login/Register

---
+++ src/challenge/users/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+
+# Create your views here.
+def user_signin(request):
+
+    if request.POST:
+        email = request.POST.get("email", "")
+        password = request.POST.get("password", "")
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            login(request, user)
+
+        return render(request, "index.html", {})
+    else:
+        return render(request, "index.html", {})
+
+def user_signup(request):
+    if request.POST:
+        email = request.POST.get("email","")
+        password = request.POST.get("password","")
+        print(email, password)
+        User.objects.create_user(email, email=email, password=password)
+        return render(request, "index.html", {"signup_complete": True})
+    else:
+        return render(request, "index.html", {"signup": True})
+
+def user_signout(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect("/")
